467c840d20d5ad6ba418d6b46b1adaae55e2d2cf
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2023-10-09 22:53:47 +0800

    SettingMyFirstPage
    setting my first page file and making myfirstpage as the home for the application

---
+++ flutter-doctor-app-main/starter/doctor_app/lib/components/first_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_doctor_app/components/app_bar_component.dart';
+import 'package:flutter_doctor_app/components/grid_icons.dart';
+import 'package:flutter_doctor_app/components/search_bar.dart';
+import 'package:flutter_doctor_app/components/second_page.dart';
+import 'package:flutter_doctor_app/components/doctor_tab.dart';
+
+class MyFirstPage extends StatefulWidget {
+  const MyFirstPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyFirstPage> createState() => _MyFirstPageState();
+}
+
+class _MyFirstPageState extends State<MyFirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
