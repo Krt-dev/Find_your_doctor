481556d7387c9de222ff4951690f8ead7c500cdb
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2023-10-10 22:15:38 +0800

    Final grid Icons

---
+++ flutter-doctor-app-main/starter/doctor_app/lib/components/data.dart
@@ -0,0 +1,10 @@
+final List<Map> data = [
+  {'id': 1, 'link': 'assets/svg/img-consultation.svg', 'title': 'Consultation'},
+  {'id': 2, 'link': 'assets/svg/img-dental.svg', 'title': 'Dental'},
+  {'id': 3, 'link': 'assets/svg/img-heart.svg', 'title': 'Heart'},
+  {'id': 4, 'link': 'assets/svg/img-hospital.svg', 'title': 'Hospital'},
+  {'id': 5, 'link': 'assets/svg/img-medicine.svg', 'title': 'Medecines'},
+  {'id': 6, 'link': 'assets/svg/img-physician.svg', 'title': 'Physician'},
+  {'id': 7, 'link': 'assets/svg/img-skin.svg', 'title': 'Skin'},
+  {'id': 8, 'link': 'assets/svg/img-surgeon.svg', 'title': 'Surgeon'},
+];
--- flutter-doctor-app-main/starter/doctor_app/lib/components/first_page.dart
+++ flutter-doctor-app-main/starter/doctor_app/lib/components/first_page.dart
@@ -17,58 +17,13 @@ class MyFirstPage extends StatefulWidget {
 class _MyFirstPageState extends State<MyFirstPage> {
   @override
   Widget build(BuildContext context) {
-    final List<Map> data = [
-      {
-        'id': 1,
-        'link': 'assets/svg/img-consultation.svg',
-        'title': 'Consultation'
-      },
-      {'id': 2, 'link': 'assets/svg/img-dental.svg', 'title': 'Dental'},
-      {'id': 3, 'link': 'assets/svg/img-heart.svg', 'title': 'Heart'},
-      {'id': 4, 'link': 'assets/svg/img-hospital.svg', 'title': 'Hospital'},
-      {'id': 5, 'link': 'assets/svg/img-medicine.svg', 'title': 'Medecines'},
-      {'id': 6, 'link': 'assets/svg/img-physician.svg', 'title': 'Physician'},
-      {'id': 7, 'link': 'assets/svg/img-skin.svg', 'title': 'Skin'},
-      {'id': 8, 'link': 'assets/svg/img-surgeon.svg', 'title': 'Surgeon'},
-    ];
-    return Column(children: [
-      const AppBarComponent(),
-      const Padding(
+    return const Column(children: [
+      AppBarComponent(),
+      Padding(
         padding: EdgeInsets.only(top: 24),
         child: Material(child: SearchingBar()),
       ),
-      SizedBox(
-        height: 300,
-        width: 500,
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4),
-          itemCount: 8,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Container(
-                    height: 56,
-                    width: 56,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                            image: Svg(data[index]['link']),
-                            fit: BoxFit.cover)),
-                  ),
-                  Text(
-                    data[index]['title'],
-                    style: GoogleFonts.lato(
-                        fontSize: 12, fontWeight: FontWeight.w400),
-                  )
-                ],
-              ),
-            );
-          },
-        ),
-      )
+      GridIcons(),
     ]);
   }
 }
